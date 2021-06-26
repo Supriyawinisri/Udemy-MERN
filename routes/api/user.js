81e27ac1ef749f8f25bdca97b3e93cdff5302c84
Shanmugapriya <shanmr17@in.ibm.com>
2021-06-26 16:09:38 +0530

    register user and get jwt token

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require('express-validator');
 const User = require('../../model/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //test route
 userRouter.get('/', (req, res) => { res.send(`User route`); })
@@ -33,7 +35,15 @@ userRouter.post('/', [
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt)
         await user.save();
-        res.send(`User registered`);
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), (err, token) => {
+            if (err) throw err;
+            res.json({ token })
+        })
     } catch (error) {
         return res.status(500).json({ msg: `Server error` });
     }
