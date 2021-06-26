cd30e188ae95ae14cd4efb55478e41802d79ddfe
Shanmugapriya <supriyawinisri1012@gmail.com>
2021-06-26 12:53:55 +0530

    Add files via upload
    Server created using express

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/',(req, res) => {res.send('App Running')})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
